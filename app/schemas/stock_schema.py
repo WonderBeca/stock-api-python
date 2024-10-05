2f8e46189ebb3f5ccd6f5a2cf93a02624f4a93d3
Rebeca <rebeca@DESKTOP-K7TCLUN.localdomain>
2024-10-05 19:17:24 -0300

    create basic schemas

--- app/schemas/stock_schema.py
+++ app/schemas/stock_schema.py
@@ -1,15 +1,41 @@
-# app/schemas/stock_schema.py
-
 from pydantic import BaseModel
+from typing import List, Optional
+
+class StockValues(BaseModel):
+    open: float
+    high: float
+    low: float
+    close: float
+
+class PerformanceData(BaseModel):
+    five_days: Optional[float]
+    one_month: Optional[float]
+    three_months: Optional[float]
+    year_to_date: Optional[float]
+    one_year: Optional[float]
+
+class Competitor(BaseModel):
+    name: str
+    market_cap: dict
 
 class StockBase(BaseModel):
-    name: str
-    price: int
+    status: str
+    purchased_amount: int
+    purchased_status: str
+    request_data: str
+    company_code: str
+    company_name: str
+    stock_values: StockValues
+    performance_data: PerformanceData
+    competitors: List[Competitor]
 
 class StockCreate(StockBase):
     pass
 
-class Stock(StockBase):
+class StockUpdate(BaseModel):
+    purchased_amount: int
+
+class StockResponse(StockBase):
     id: int
 
     class Config:
