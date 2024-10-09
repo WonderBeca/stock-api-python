036e3c5073fc2bc85892d78d859fcb2d98418cf1
Rebeca <rebeca@DESKTOP-K7TCLUN.localdomain>
2024-10-09 17:58:23 -0300

    flake8 adjustments

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from app.database.database import Base
 
+
 @pytest.fixture(scope="session")
 async def db():
     engine = create_async_engine("sqlite+aiosqlite:///:memory:", echo=True)
@@ -12,14 +13,10 @@ async def db():
         expire_on_commit=False,
     )
 
-    # Criação das tabelas
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-    # Retorno da sessão
     async with async_session() as session:
-        yield session  # Retorna a sessão para os testes
-
-        # Limpeza do banco de dados após os testes
+        yield session
         await session.close()
         await engine.dispose()
--- tests/test_user_routes.py
+++ tests/test_user_routes.py
@@ -3,7 +3,6 @@ from unittest.mock import patch
 from fastapi.testclient import TestClient
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.api.user_routes import router
-from app.database.database import get_db
 from app.models.user_model import User
 from app.utils.auth_utils import create_access_token
 from fastapi import HTTPException
@@ -15,21 +14,23 @@ def client():
     with TestClient(router) as c:
         yield c
 
+
 @pytest.fixture
 async def test_user(db: AsyncSession, username="testuser", password="hashed_password"):
-    # Tente encontrar o usuário existente
     existing_user = await db.execute(select(User).filter_by(username=username))
     user = existing_user.scalar_one_or_none()
 
-    # Se o usuário não existir, crie um novo
     if user is None:
         new_user = User(username=username, hashed_password=password)
         db.add(new_user)
         await db.commit()
         await db.refresh(new_user)
-        return new_user
+        yield new_user
+        await db.delete(new_user)
+        await db.commit()
+    else:
+        yield user
 
-    return user  # Retorne o usuário existente
 
 @pytest.mark.asyncio
 async def test_home(client):
@@ -37,11 +38,13 @@ async def test_home(client):
     assert response.status_code == 200
     assert "Hey!" in response.text
 
+
 @pytest.mark.asyncio
 async def test_register_form(client):
     response = client.get("/register")
     assert response.status_code == 200
     assert "Register" in response.text
+
 
 @pytest.mark.asyncio
 async def test_login_form(client):
@@ -54,19 +57,21 @@ async def test_login_form(client):
 async def test_welcome(client, test_user):
     access_token = create_access_token(data={"sub": test_user.username})
     with pytest.raises(HTTPException) as exc_info:
-        response = client.get("/welcome", cookies={"access_token": access_token})
+        client.get("/welcome", cookies={"access_token": access_token})
     assert exc_info.value.detail == "Redirecting to login page"
+
 
 @pytest.mark.asyncio
 async def test_welcome_without_auth(client):
     headers = {"content-type": "application/json"}
-    
+
     with pytest.raises(HTTPException) as exc_info:
         await client.get("/welcome", headers=headers)
-    
+
     # Verifique o código de status e a mensagem de erro
     assert exc_info.value.status_code == 401
     assert exc_info.value.detail == "Not authenticated"
+
 
 @pytest.mark.asyncio
 @patch('app.api.user_routes.get_user_by_username')
@@ -80,12 +85,13 @@ async def test_login_success(mock_create_token, mock_verify_pass, mock_get_user,
         "username": "testuser",
         "password": "hashed_password"
     }
-    
+
     response = client.post("/login", json=login_data)
-    
+
     assert response.status_code == 200
     assert "access_token" in response.json()
     assert response.json()["access_token"] == "mocked_access_token"
+
 
 @pytest.mark.asyncio
 @patch('app.api.user_routes.get_user_by_username')
@@ -98,8 +104,8 @@ async def test_login_fail(mock_create_token, mock_verify_pass, mock_get_user, cl
         "username": "wrong_user",
         "password": "wrong_pass"
     }
-    
+
     response = client.post("/login", json=login_data)
-    
+
     assert response.status_code == 400
     assert "access_token" not in response.json()
