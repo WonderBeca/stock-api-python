69ddda8be03192212d2dd5db3579519d1195ab84
Rebeca <rebeca@DESKTOP-K7TCLUN.localdomain>
2024-10-09 18:39:48 -0300

    flake8

--- app/database/database.py
+++ app/database/database.py
@@ -1,11 +1,7 @@
-# app/database/database.py
-
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.future import select
-from sqlalchemy import text
 import os
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -22,10 +18,12 @@ AsyncSessionLocal = sessionmaker(
 
 Base = declarative_base()
 
+
 # Function to initialize the database
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
 
 async def get_db():
     async with AsyncSessionLocal() as session:
